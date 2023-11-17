903809b6cbccd024221b78f12e0edacf9efc4ec7
Mokona Madoki <ddiimaas@mail.ru>
2023-11-17 16:17:21 +0300

    приветствие

---
+++ Exemple005_If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name");
+string user name = Console.ReadLine;
+
+if(username == "Masha")
+{
+    Console.WriteLine("Go play dota2");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
