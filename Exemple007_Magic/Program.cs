6e02cb811891da3d6c400852ebd8cbba827cbab2
Mokona Madoki <ddiimaas@mail.ru>
2023-11-22 14:36:52 +0300

    ыфвф

---
+++ Exemple007_Magic/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
+
+int xa = 10;
+int ya = 1;
+
+int xb = 1;
+int yb = 20;
+
+int xc = 10;
+int yc = 40;
+
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("-");
+
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("*");
