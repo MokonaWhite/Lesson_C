546f62da34d0fef81f63414ccecc6ef06fbd4cf8
Mokona Madoki <ddiimaas@mail.ru>
2023-11-24 10:18:39 +0300

    вносим изменения

---
+++ Exemple010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 31, 4, 15, 16, 17, 18 };
+
+int num = array.Length; 
+int find = 31;
+int index = 0;
+
+while (index < num)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1
+    index++;
+}
